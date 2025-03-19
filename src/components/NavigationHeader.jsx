d2e685ee899d8386a5cb98312f2cdddd8298fb5e
jwong65 <jason.wong6507@gmail.com>
2025-03-19 13:41:44 -0400

    Changed the name of the svg file

--- src/components/NavigationHeader.jsx
+++ src/components/NavigationHeader.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-
 import './NavigationHeader.css'
 import { Button } from 'antd'
+
+import { useNavigate, useLocation, Link } from 'react-router-dom'
 
 export default function NavigationHeader() {
   return (
     <header className='navigation-header'>
       <div className='brand-section'>
         <div className='logo-container'>
-          <img src='/Layer_1.svg' alt='Logo' className=''/>
+          <img src='/thunder_monetize_logo.svg' alt='Logo' className=''/>
         </div>
         <div className='company-name'>
           Thunder Monetize
