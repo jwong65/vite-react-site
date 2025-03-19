a448dffd19bd34d36ac702310a4f836eb1a25483
jwong65 <jason.wong6507@gmail.com>
2025-03-19 15:09:46 -0400

    Adjusted the styling on tabs. Put tabs in for Navigation Header

--- src/components/NavigationHeader.jsx
+++ src/components/NavigationHeader.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './NavigationHeader.css'
-import { Button } from 'antd'
+import { Tabs, Button } from 'antd'
 
-import { useNavigate, useLocation, Link } from 'react-router-dom'
-
+import { useNavigate, useLocation } from 'react-router-dom'
 export default function NavigationHeader() {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const handleTabChange = (key) =>{
+    navigate(key)
+  }
+
+  const handleContactClick = () => {
+    navigate('/contact');
+  }
+
+
   return (
     <header className='navigation-header'>
       <div className='brand-section'>
@@ -15,15 +28,23 @@ export default function NavigationHeader() {
           Thunder Monetize
         </div>
       </div>
-      <nav className='nav-links'>
-        <ul>
-          <ul> <a href='/'>Solutions</a> </ul>
-          <ul> <a href='/publisher'>For Publishers</a></ul>
-          <ul> <a href='/'>For Advertisers</a></ul>
-          <ul> <a href='/'>About Us</a></ul>
-          <ul> <Button type='primary'>Contact Us</Button></ul>
-        </ul>
-      </nav>
+      <div className='nav-tabs-container'>
+        <Tabs
+          className = 'navigation-tabs'
+          activeKey={currentPath}
+          onChange={handleTabChange}
+        >
+          <Tabs.TabPane tab="Solutions" key="/" />
+          <Tabs.TabPane tab="For Publishers" key="/publisher" />
+          <Tabs.TabPane tab="For Advertisers" key="/" />
+          <Tabs.TabPane tab="About" key="/" />
+          
+        </Tabs>
+        
+        <Button type='primary' className='contact-button' onClick={handleContactClick}>
+              Contact Us
+        </Button>
+      </div>
     </header>
    )
 }
