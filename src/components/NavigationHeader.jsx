6734d99bece13d0eace8ce1463be435d3c8fe7ea
jwong65 <jason.wong6507@gmail.com>
2025-03-19 12:28:43 -0400

    Changes to header, added logo to public instead of assets.

--- src/components/NavigationHeader.jsx
+++ src/components/NavigationHeader.jsx
@@ -6,13 +6,18 @@ import { Button } from 'antd'
 export default function NavigationHeader() {
   return (
     <header className='navigation-header'>
-      <nav>
+      <div className='logo-container'>
+        <img src='/Layer_1.svg' alt='Logo' className=''/>
+      </div>
+      <nav className='nav-links'>
         <ul>
-          <ul> <a href='/'>Home</a> </ul>
-          <ul> <a href='/about'>About</a></ul>
+          <ul> <a href='/'>Solutions</a> </ul>
+          <ul> <a href='/'>For Publishers</a></ul>
+          <ul> <a href='/'>For Advertisers</a></ul>
+          <ul> <a href='/'>About Us</a></ul>
           <ul> <Button type='primary'>Contact Us</Button></ul>
         </ul>
       </nav>
     </header>
-  )
+   )
 }
