fd70e4984d57b4b9c4859f70756f4ba4c1327d73
jwong65 <jason.wong6507@gmail.com>
2025-03-20 11:55:56 -0400

    Added inital footer file, and layout import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
-
+import { Layout } from 'antd'
 import NavigationHeader from './components/NavigationHeader'
 import Home from './pages/Home'
 import './App.css'
