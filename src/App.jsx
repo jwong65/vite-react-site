c31119c59ba4119b07ca89339b7b71dcd81c2526
jwong65 <jason.wong6507@gmail.com>
2025-03-20 12:04:42 -0400

    Removed testing border and added header wraps around NavHeader

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-
-import { Layout } from 'antd'
 import NavigationHeader from './components/NavigationHeader'
 import Home from './pages/Home'
 import Publisher from './pages/Publisher'
 import Advertisers from './pages/Advertisers'
 import About from './pages/About'
 import './App.css'
+
+
+import { Layout } from 'antd'
+const {Header, Content, Footer} =  Layout
 function App() {
 
   return (
     <Router>
-      <div className='App'>
-        <NavigationHeader />
-        <main>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/publisher' element={<Publisher />} />
-            <Route path='/advertisers' element={<Advertisers />} />
-            <Route path='/about' element={<About />} />
-          </Routes>
-        </main>
-      </div>
+      <Layout className='app-layout'>
+        <div className='App'>
+          <Header className='app-header'tyle={{ padding: 0, background: 'transparent' }}>
+            <NavigationHeader />
+          </Header>
+          <main>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/publisher' element={<Publisher />} />
+              <Route path='/advertisers' element={<Advertisers />} />
+              <Route path='/about' element={<About />} />
+            </Routes>
+          </main>
+        </div>
+      </Layout>
     </Router>
   )
 }
