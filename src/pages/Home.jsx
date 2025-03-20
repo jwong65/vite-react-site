e04cd2d68978fe8efe5397bffd79cfc0017410b6
jwong65 <jason.wong6507@gmail.com>
2025-03-20 11:34:57 -0400

    Added all text for the first text-overlay

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Card } from  'antd'
 import backgroundImage from '../assets/home_photo1.svg'
 
 import "./home.css"
@@ -8,8 +7,13 @@ export default function Home() {
   return (
     <div className='home-container'>
         <img src={backgroundImage} className='background-image' />
-        <div className='text-section'>
-            Hello
+        <div className='text-overlay-1'>
+            <h1 className='main-heading'>
+                Precision. Scale. <br/>
+                Performance.
+            </h1>
+            <h3 className='sub-heading'>Because every millisecond counts.</h3>
+            <h3 className='sub-heading2'>Maximize revenue with real-time bidding, premium demand, and AI-driven optimization—built for publishers, trusted by advertisers.</h3>
         </div>
     </div>
   )
