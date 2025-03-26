35c4924a3181cd52af6892f42918221bda9f6352
jwong65 <jason.wong6507@gmail.com>
2025-03-26 17:09:58 -0400

    Added the privacy text styling

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -8,6 +8,15 @@ export default function Privacy() {
           <div className='privacy-header'>Privacy Policy</div>
           <div className='privacy-subheader'>Last Updated: September 30, 2024</div>
         </div>
+        <div className='privacy-text'>
+          <div className='text-box-privacy'>
+          This policy explains how Thunder Monetize, Inc. (“Thunder", "we" or “us”) collects, uses and discloses information about you when you interact with Digital Media Properties (defined below) that use our advertising technology platforms and related services to deliver you ads (collectively, the “Services”).
+          This privacy policy does not govern (i) the collection and use of information about visitors to our website, or (ii) information collected in the course of our sales and marketing activities. Our privacy practices as to that information can be found <a href="https://www.magnite.com/legal/magnite-website-privacy-policy/">here</a>.
+          This policy also does not govern the collection, use and disclosure of information by our clients through their Digital Media Properties, the activities of other third parties we may work with, or any activities where we act as a service provider or processor to our clients rather than a data controller. 
+          Those practices are governed by our clients' own privacy policies.
+          </div>
+          <div className='privacy-text-header'>About Us</div>
+        </div>
       </div>
   )
 }
