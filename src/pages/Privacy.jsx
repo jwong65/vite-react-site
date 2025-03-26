faab474c6d3c4aede95e9a62f884f4caa2154fba
jwong65 <jason.wong6507@gmail.com>
2025-03-26 17:22:12 -0400

    Added about us text in the privacy policy

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -10,12 +10,18 @@ export default function Privacy() {
         </div>
         <div className='privacy-text'>
           <div className='text-box-privacy'>
-          This policy explains how Thunder Monetize, Inc. (“Thunder", "we" or “us”) collects, uses and discloses information about you when you interact with Digital Media Properties (defined below) that use our advertising technology platforms and related services to deliver you ads (collectively, the “Services”).
-          This privacy policy does not govern (i) the collection and use of information about visitors to our website, or (ii) information collected in the course of our sales and marketing activities. Our privacy practices as to that information can be found <a href="https://www.magnite.com/legal/magnite-website-privacy-policy/">here</a>.
-          This policy also does not govern the collection, use and disclosure of information by our clients through their Digital Media Properties, the activities of other third parties we may work with, or any activities where we act as a service provider or processor to our clients rather than a data controller. 
-          Those practices are governed by our clients' own privacy policies.
+            This policy explains how Thunder Monetize, Inc. (“Thunder", "we" or “us”) collects, uses and discloses information about you when you interact with Digital Media Properties (defined below) that use our advertising technology platforms and related services to deliver you ads (collectively, the “Services”).
+            This privacy policy does not govern (i) the collection and use of information about visitors to our website, or (ii) information collected in the course of our sales and marketing activities. Our privacy practices as to that information can be found <a href="https://www.magnite.com/legal/magnite-website-privacy-policy/">here</a>.
+            This policy also does not govern the collection, use and disclosure of information by our clients through their Digital Media Properties, the activities of other third parties we may work with, or any activities where we act as a service provider or processor to our clients rather than a data controller. 
+            Those practices are governed by our clients' own privacy policies.
           </div>
           <div className='privacy-text-header'>About Us</div>
+          <div className='text-box-privacy'>
+            Thunder Monetize offers Services that connect buyers of advertising inventory (which we refer to as advertisers) with sellers of advertising inventory (which we refer to as publishers), providing such buyers with the opportunity to display an advertisement on a seller's Digital Media Property.
+            In this policy, we refer to our buyers and sellers collectively as “Clients”.
+            Our platform is used to deliver digital advertising inventory on websites and applications across Internet-connected devices and platforms, including mobile devices, computers and internet-connected TVs (which we collectively refer to as “Digital Media Properties”).
+          </div>
+          <div className='privacy-text-header'>How We Collect User Information</div>
         </div>
       </div>
   )
