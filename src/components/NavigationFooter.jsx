da0c5b2b4baa2af145e92f6d40df3bdbd732d728
jwong65 <jason.wong6507@gmail.com>
2025-03-26 17:11:00 -0400

    removed the link in the footer container

--- src/components/NavigationFooter.jsx
+++ src/components/NavigationFooter.jsx
@@ -8,14 +8,12 @@ export default function NavigationFooter() {
   return (
     <div className='footer-wrapper'>
         <div className='footer-container'>
-        <Link to="/" className='brand-section-footer'>
             <div className='logo-container-footer'>
               <img src={companyLogo} alt='Logo' className='logo'/>
             </div>
             <div className='company-name-footer'>
               Thunder Monetize
             </div>
-        </Link>
 
         <div className='footer-links'>
           <div className='footer-links-column'>
