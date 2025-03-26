54ad174f6db9e7f1ea5d54c2471b3ea2f4242a90
jwong65 <jason.wong6507@gmail.com>
2025-03-26 15:33:50 -0400

    Added the page-wrapper and privacy box to jsx, styled it and centered the text

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import './privacy.css'
 
 export default function Privacy() {
   return (
-    <div>Privacy</div>
+      <div className='page-wrapper'>
+        <div className='privacy-box'>
+          <div className='privacy-header'>Privacy Policy</div>
+          <div className='privacy-subheader'>Last Updated: September 30, 2024</div>
+        </div>
+      </div>
   )
 }
