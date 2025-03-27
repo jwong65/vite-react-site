ecb63b19c626eb1300c3b14957aafe958effb0dc
jwong65 <jason.wong6507@gmail.com>
2025-03-26 20:17:10 -0400

    Added the first publisher image, imported it into page

--- src/pages/Publisher.jsx
+++ src/pages/Publisher.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import publisherImage from "../assets/publisher_photo1.png"
 
 export default function Publisher() {
   return (
-    <div>Publisher</div>
+    <div className='publisher-container'>
+      <div className='publisher-hero-container'>
+        <img src={publisherImage} className='background-publisher' />
+
+      </div>
+      Publisher
+    </div>
   )
 }
