79482fa324699aff298ff0a1ef25533f6915cdcf
jwong65 <jason.wong6507@gmail.com>
2025-03-27 13:29:51 -0400

    Added the second container for publisher page, styled logo

--- src/pages/Publisher.jsx
+++ src/pages/Publisher.jsx
@@ -11,27 +11,35 @@ export default function Publisher() {
   return (
     <div className='publisher-page-wrapper'>
       <div className = 'publisher-hero-section-wrapper'>
-      <div className='publisher-hero-container'>
-        <img src={publisherImage} className='background-publisher' />
-        <img src={publisherImage1} className='publisher-floating-overlay' />
-        <div className='publisher-text-overlay'>
-          <h1 className='publisher-main-heading'>
-            Publisher Solutions
-          </h1>
-          <h3 className='publisher-sub-heading'>
-            Maximize Revenue. <br/>
-            Minimize Hassle.
-          </h3>
-          <h3 className='publisher-sub-heading2'>
-          Your inventory deserves top-tier demand and real-time optimizations that work as fast as your users.
-          Thunder Monetize delivers powerful monetization solutions, AI-driven yield optimization, and premium ad formats to maximize every impression—so you earn more with zero wasted potential.
-          </h3>
-          <Link to='/contact'>
-            < Button type='primary'>Supercharge Your Growth Today</Button>
-          </Link>
+        <div className='publisher-hero-container'>
+          <img src={publisherImage} className='background-publisher' />
+          <img src={publisherImage1} className='publisher-floating-overlay' />
+          <div className='publisher-text-overlay'>
+            <h1 className='publisher-main-heading'>
+              Publisher Solutions
+            </h1>
+            <h3 className='publisher-sub-heading'>
+              Maximize Revenue. <br/>
+              Minimize Hassle.
+            </h3>
+            <h3 className='publisher-sub-heading2'>
+            Your inventory deserves top-tier demand and real-time optimizations that work as fast as your users.
+            Thunder Monetize delivers powerful monetization solutions, AI-driven yield optimization, and premium ad formats to maximize every impression—so you earn more with zero wasted potential.
+            </h3>
+            <Link to='/contact'>
+              < Button type='primary'>Supercharge Your Growth Today</Button>
+            </Link>
+          </div>
         </div>
-
       </div>
+      <div className='publisher-container-2-wrapper'>
+        <div className='publisher-container-2'>
+          <img src='/thunder_monetize_logo.svg' alt='Company Logo' className='company-logo'/>
+          <div className='publisher-text-overlay-2'>
+            We're not just another ad exchange—we're your profitability partner.
+            With cutting-edge traffic shaping, seamless integrations, and premium advertiser demand, we ensure higher fill rates, increased eCPMs, and smarter monetization at scale.
+          </div>
+        </div>
       </div>
       Publisher
     </div>
