0b1eb67ff5fd7c830384581f77c3ba5dc7df1803
jwong65 <jason.wong6507@gmail.com>
2025-03-27 19:00:21 -0400

    Added the photo1 and started the page wrapper for about component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import aboutImage from "../assets/about_photo1.png"
 export default function About() {
   return (
-    <div>About</div>
+    <div className='about-page-wrapper'>
+      <div className='about-hero-section-wrapper'>
+        <div className='about-hero-container'>
+          <img src={aboutImage} className='about-background-image' />
+        </div>
+      </div>
+      About
+    </div>
   )
 }
