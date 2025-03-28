d80734d4397b9a62e65876052571ce237fdb5edf
jwong65 <jason.wong6507@gmail.com>
2025-03-28 19:16:56 -0400

    Added the inital files and 2 photos, linked CSS

--- src/pages/Advertisers.jsx
+++ src/pages/Advertisers.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import "./advertisers.css"
+import advertiserPhoto from "../assets/advertiser_photo1.png"
+import advertiserPhoto2 from "../assets/advertiser_photo2.png"
+
 export default function Advertisers() {
   return (
-    <div>Advertisers</div>
+    <div className='advertiser-page-wrapper'>
+      <div className='advertiser-hero-section-wrapper'>
+        <div className='advertiser-hero-container'></div>
+        <img src={advertiserPhoto} className='background-advertiser'/>
+        <img src= {advertiserPhoto2} className='advertiser-floating-overlay' />
+      </div>
+      Advertisers
+    </div>
   )
 }
