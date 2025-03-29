c098389d556f673647ea964eb46d5df25b545a37
jwong65 <jason.wong6507@gmail.com>
2025-03-29 19:16:50 -0400

    Added text for the hero container

--- src/pages/Advertisers.jsx
+++ src/pages/Advertisers.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import "./advertisers.css"
 import advertiserPhoto from "../assets/advertiser_photo1.png"
@@ -8,9 +9,23 @@ export default function Advertisers() {
   return (
     <div className='advertiser-page-wrapper'>
       <div className='advertiser-hero-section-wrapper'>
-        <div className='advertiser-hero-container'></div>
-        <img src={advertiserPhoto} className='background-advertiser'/>
-        <img src= {advertiserPhoto2} className='advertiser-floating-overlay' />
+        <div className='advertiser-hero-container'>
+          <img src={advertiserPhoto} className='background-advertiser'/>
+          <img src= {advertiserPhoto2} className='advertiser-floating-overlay' />
+        </div>
+        <div className='advertiser-text-overlay'>
+          <h1 className='advertiser-main-heading'>
+            Advertiser Solutions 
+          </h1>
+          <h3 className='advertiser-sub-heading'>
+            Lightning-Fast <br/>
+            Reach. <br />
+            Precision Targeting.
+          </h3>
+          <h3 className='advertiser-sub-heading2'>
+            Reach premium audiences with full transparency and precision. Thunder Monetize connects you to high-quality, brand-safe inventory while leveraging advanced optimization and fraud prevention tools to ensure every ad dollar drives real results.
+          </h3>
+        </div>
       </div>
       Advertisers
     </div>
