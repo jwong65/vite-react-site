3c9b827dd4e1636c6b96812a34487c1ea99b03f9
jwong65 <jason.wong6507@gmail.com>
2025-03-29 21:30:26 -0400

    Added styling for hero-container

--- src/pages/Advertisers.jsx
+++ src/pages/Advertisers.jsx
@@ -13,22 +13,22 @@ export default function Advertisers() {
         <div className='advertiser-hero-container'>
           <img src={advertiserPhoto} className='background-advertiser'/>
           <img src= {advertiserPhoto2} className='advertiser-floating-overlay' />
-        </div>
-        <div className='advertiser-text-overlay'>
-          <h1 className='advertiser-main-heading'>
-            Advertiser Solutions 
-          </h1>
-          <h3 className='advertiser-sub-heading'>
-            Lightning-Fast <br/>
-            Reach. <br />
-            Precision Targeting.
-          </h3>
-          <h3 className='advertiser-sub-heading2'>
-            Reach premium audiences with full transparency and precision. Thunder Monetize connects you to high-quality, brand-safe inventory while leveraging advanced optimization and fraud prevention tools to ensure every ad dollar drives real results.
-          </h3>
-          <Link to='/contact'>
-            < Button type='primary'>Maximize Your Impact</Button>
-          </Link>
+          <div className='advertiser-text-overlay'>
+            <h1 className='advertiser-main-heading'>
+              Advertiser Solutions 
+            </h1>
+            <h3 className='advertiser-sub-heading'>
+              Lightning-Fast <br/>
+              Reach. <br />
+              Precision Targeting.
+            </h3>
+            <h3 className='advertiser-sub-heading2'>
+              Reach premium audiences with full transparency and precision. Thunder Monetize connects you to high-quality, brand-safe inventory while leveraging advanced optimization and fraud prevention tools to ensure every ad dollar drives real results.
+            </h3>
+            <Link to='/contact'>
+              < Button type='primary'>Maximize Your Impact</Button>
+            </Link>
+          </div>
         </div>
       </div>
       Advertisers
