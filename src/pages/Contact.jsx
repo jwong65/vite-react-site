6948f1f0452a84062c383a8a888dbba6613f213d
jwong65 <jason.wong6507@gmail.com>
2025-03-31 03:05:18 -0400

    Added the image to the contact page, added the text for the contact-text-overlay

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import contactImage from "../assets/contact_photo1.png"
+
+import "./contact.css"
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact-page-wrapper'>
+      <div className='contact-container'>
+        <img src={contactImage} className='contact-background-image' />
+        <div className='contact-text-overlay'>
+          <h1 className='contact-main-heading'>
+            Supercharge Your Growth
+          </h1>
+          <h3 className='contact-sub-heading'>
+            Let's Talk
+          </h3>
+          <h3 className='contact-sub-heading2'>
+            Whether you're a publisher looking to maximize revenue or an advertiser seeking premium inventory, Thunder Monetize is here to deliver results. Reach out and let’s make every millisecond count.
+          </h3>
+        </div>
+      </div>
+      Contact
+    </div>
   )
 }
