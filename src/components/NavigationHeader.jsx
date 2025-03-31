255a0b625bbb921f75cfc16d83ebf2ee423fab69
jwong65 <jason.wong6507@gmail.com>
2025-03-31 07:31:21 -0400

    To avoid confusion changed it to logo-container-header

--- src/components/NavigationHeader.jsx
+++ src/components/NavigationHeader.jsx
@@ -86,7 +86,7 @@ export default function NavigationHeader() {
           )
         }
           <Link to="/" className='brand-section'>
-            <div className='logo-container-home'>
+            <div className='logo-container-header'>
               <img src='/thunder_monetize_logo.svg' alt='Logo' className='logo'/>
             </div>
             <div className='company-name'>
