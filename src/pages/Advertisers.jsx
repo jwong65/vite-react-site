33f064fd256aced9d3facf99262a2109757db31d
jwong65 <jason.wong6507@gmail.com>
2025-04-03 03:05:48 -0400

    Removed the button container in advetiser-container-4-wrapper as it is not aligned the same

--- src/pages/Advertisers.jsx
+++ src/pages/Advertisers.jsx
@@ -131,11 +131,9 @@ export default function Advertisers() {
             <div className='advertiser-text-wrapper-header'>Maximize Impact with Thunder.</div>
             <div className='advertiser-text-wrapper-subheader'>Join the world's leading advertisers in a faster, safer, and more transparent ecosystem. Let's get started.</div>
           </div>
-          <div className='button-container'>
-            <Link to='/contact'>
-                <Button type='primary' className='advertiser-custom-button'>Supercharge Your Campaigns Today</Button>
-            </Link>
-          </div>
+          <Link to='/contact'>
+              <Button type='primary' className='advertiser-custom-button'>Supercharge Your Campaigns Today</Button>
+          </Link>
         </div>
       </div>
     </div>
